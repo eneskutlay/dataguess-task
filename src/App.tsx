95c5fd0f611e40f6616b9c668df32e40452d3dae
Enes Kutlay <hi@eneskutlay.dev>
2023-09-12 13:39:38 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Junior Frontend Developer Assignment</h1>
+      </header>
+    </div>
+  );
+}
