96ab27632ddb63a6e6356056a35156da09516fcf
Enes Kutlay <hi@eneskutlay.dev>
2023-09-12 14:17:48 +0300

    Render test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+//import { useQuery, gql } from '@apollo/client';
+import CountryList from "./components/CountryList.tsx";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Junior Frontend Developer Assignment</h1>
+        <CountryList />
       </header>
     </div>
   );
