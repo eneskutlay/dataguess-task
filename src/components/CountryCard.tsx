74639bbb0ff5fac7b3108458b20854a938163923
Enes Kutlay <hi@eneskutlay.dev>
2023-09-14 15:37:29 +0300

    Style fix

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
 import "./styles/CountryCard.css";
 
 interface CountryCardProps {
@@ -7,7 +8,11 @@ interface CountryCardProps {
   emoji: string;
   currency: string;
   languages: { code: string; name: string }[];
+  isOpen: boolean;
+  toggleCard: () => void;
 }
+
+const countryDataNotFound = "Data not available";
 
 const CountryCard: React.FC<CountryCardProps> = ({
   name,
@@ -15,32 +20,31 @@ const CountryCard: React.FC<CountryCardProps> = ({
   emoji,
   currency,
   languages,
+  isOpen,
+  toggleCard,
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleCard = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <div className="countryCard">
-      <div className="countryHeader" onClick={toggleCard}>
+    <div className={`countryCard ${isOpen ? "open" : ""}`} onClick={toggleCard}>
+      <div className="countryHeader">
         <h4>{name}</h4>
       </div>
       {isOpen && (
         <div className="countryDetails">
           <p>
-            <strong>Capital:</strong> {capital}
+            <strong>Capital:</strong> {capital ? capital : countryDataNotFound}
           </p>
           <p>
             <strong>Emoji:</strong> {emoji}
           </p>
           <p>
-            <strong>Currency:</strong> {currency}
+            <strong>Currency:</strong>{" "}
+            {currency ? currency : countryDataNotFound}
           </p>
           <p>
             <strong>Languages:</strong>{" "}
-            {languages.map((lang) => lang.name).join(", ")}
+            {languages.map((lang) => lang.name).join(", ")
+              ? languages.map((lang) => lang.name).join(", ")
+              : countryDataNotFound}
           </p>
         </div>
       )}
